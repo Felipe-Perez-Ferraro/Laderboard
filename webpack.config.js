a92c979fce9cf853e6a2f80a27cbcec8b90cadf9
Feliperto <fperezferraro@gmail.com>
2023-07-25 13:28:54 -0300

    fixing linters errors

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
   },
   optimization: {
     minimize: true,
